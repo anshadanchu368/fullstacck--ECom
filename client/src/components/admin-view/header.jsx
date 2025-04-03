ff3671a3f63ed04205de2b47754c62844511a306
Hashim Anhad <anshadanchu368@gmail.com>
2025-04-03 19:52:06 +0530

    sidebar finsished

--- client/src/components/admin-view/header.jsx
+++ client/src/components/admin-view/header.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Button } from '../button'
+import { AlignJustify, LogOut } from 'lucide-react'
 
-const AdminHeader = () => {
+const AdminHeader = ({setOpen}) => {
   return (
-    <div>
-      header
-    </div>
+  <header className="flex justify-between items-center px-4 py-3">
+      <Button onClick={()=> setOpen(true)} className="lg:hidden sm:block">
+         <AlignJustify/>
+         <span className="sr-only">Toggle Menu</span>
+      </Button>
+      <div className="flex flex-1 justify-end">
+        <Button className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow">
+        <LogOut />
+        Logout
+        </Button>
+      </div>
+  </header>
   )
 }
 
