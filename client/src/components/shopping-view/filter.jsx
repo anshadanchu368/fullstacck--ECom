ae02c6bb9e5bbf00ffc5d2232c9fd9970930ee92
Hashim Anhad <anshadanchu368@gmail.com>
2025-04-06 17:16:14 +0530

    sort ui

--- client/src/components/shopping-view/filter.jsx
+++ client/src/components/shopping-view/filter.jsx
@@ -7,7 +7,7 @@ const ProductFilter = () => {
   return (
     <div className="bg-background rounded-lg shadow-sm">
       <div className="p-4 border-b">
-        <h2 className="text-lg font-extraibold">Filter</h2>
+        <h2 className="text-lg font-extrabold">Filter</h2>
       </div>
       <div className="p-4 space-y-4 ">
         {Object.keys(filterOptions).map((keyItem) => {
