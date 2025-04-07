f09c78da3d3cb4272acb53084a748033ba1965a4
Hashim Anhad <anshadanchu368@gmail.com>
2025-04-08 00:30:43 +0530

    dialog Product details page

--- client/src/pages/shopping-view/product-details.jsx
+++ client/src/pages/shopping-view/product-details.jsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/button";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
-import { IndianRupee } from "lucide-react";
+import { IndianRupee, StarIcon, StarsIcon } from "lucide-react";
 
 function ProductsDetailsDialog({ open, setOpen, productDetails }) {
+    
+      
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent className="grid grid-cols-2 gap-8 sm:p-12 max-w-[90vw] sm:max-w-[80vw] lg:max-w-[70vw]">
@@ -23,6 +27,7 @@ function ProductsDetailsDialog({ open, setOpen, productDetails }) {
               {productDetails?.description}
             </p>
           </div>
+
           <div className="flex items-center justify-between">
             <p
               className={`${
@@ -37,13 +42,46 @@ function ProductsDetailsDialog({ open, setOpen, productDetails }) {
               </p>
             ) : null}
           </div>
-          <div className="mt-5">
+          <div className="flex items-center hap-2 mt-2">
+          <div className="flex items-center gap-0 5">
+                        <StarIcon className="w-5 h-5 fill-primary"/>
+                        <StarIcon className="w-5 h-5 fill-primary"/>
+                        <StarIcon className="w-5 h-5 fill-primary"/>
+                        <StarIcon className="w-5 h-5 fill-primary"/>
+                        <StarIcon className="w-5 h-5 fill-primary"/>
+                    </div>
+                    <span className="text-muted-foreground"> 4.5</span>
+          </div>
+          <div className="mt-5 mb-5">
             <Button className="w-full">Add to Cart</Button>
           </div>
           <Separator className=" mt-5 mb-5"/>
         <div className="max-h-[300px] overflow-auto">
          <h2 className="text-xl font-bold">Reviews</h2>
-         
+         <div className="grid gap-6">
+              <div className="flex gap-4">
+                   <Avatar className ="w-10 h-10 border">
+                         <AvatarFallback>SM</AvatarFallback>
+                   </Avatar>
+                   <div className="grid gap-1">
+                    <div className="flex items-center gap-2">
+                        <h3 className="font-bold">Sangam Mukherjee</h3>
+                    </div>
+                    <div className="flex items-center gap-0 5">
+                        <StarIcon className="w-5 h-5 fill-primary"/>
+                        <StarIcon className="w-5 h-5 fill-primary"/>
+                        <StarIcon className="w-5 h-5 fill-primary"/>
+                        <StarIcon className="w-5 h-5 fill-primary"/>
+                        <StarIcon className="w-5 h-5 fill-primary"/>
+                    </div>
+                    <p className="text-muted-foreground">This is an awesome product</p>
+                   </div>
+              </div>
+         </div>
+          <div className="mt-6 flex gap-2">
+            <Input placeholder="Write a review"/>
+            <Button>Submit</Button>
+          </div>
         </div>
         </div>
       </DialogContent>
